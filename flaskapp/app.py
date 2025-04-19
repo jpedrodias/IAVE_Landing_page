2cacba26a1fa5018eccb00dcacc3629830ade331
Pedro Dias <jpedrodias@gmail.com>
2025-04-19 14:28:08 +0100

    flaskapp

---
+++ flaskapp/app.py
@@ -0,0 +1,162 @@
+import os
+import functools
+import datetime
+
+from flask import Flask, request, make_response, render_template, redirect, url_for, session, send_file, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///myapp.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('FLASKAPP_SECRET_KEY', 'mysecret')
+app.config['SESSION_TYPE'] = 'filesystem'
+
+try:
+    app.config['DEBUG'] = bool(int(os.getenv('FLASKAPP_DEBUG', 0)))
+except:
+    app.config['DEBUG'] = False
+
+app.config['REDIRECT_URL'] = os.getenv('FLASKAPP_REDIRECT_URL', 'http://provas.iave.pt')
+app.config['REDIRECT_WAIT'] = os.getenv('FLASKAPP_REDIRECT_WAIT', 10)
+app.config['DOWNLOAD_FOLDER'] = os.getenv('FLASKAPP_DOWNLOAD_FOLDER', 'download')
+
+if app.config['DEBUG']:
+    print(app.config)
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Record(db.Model):
+    __tablename__ = 'records'
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    ip_address = db.Column(db.String(45), nullable=False)
+    user_agent = db.Column(db.String(256), nullable=False)
+#end class
+
+
+def no_cache(view):
+    @functools.wraps(view)
+    def no_cache_wrapper(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '0'
+        return response
+    return no_cache_wrapper
+
+
+
+@app.route('/')
+@no_cache
+def homepage():
+    ip_address = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    record = Record(ip_address=ip_address, user_agent=user_agent)
+    db.session.add(record)
+    db.session.commit()
+    return render_template('index.html',
+        redirect=app.config.get('REDIRECT_URL'),
+        wait=app.config.get('REDIRECT_WAIT'),
+        ip_address=ip_address,
+        user_agent=user_agent
+    )
+
+
+@app.route('/download/')
+@app.route('/download/<path:filename>')
+def download(filename=None):
+    if not filename:
+        return render_template('download.html')
+    
+    if filename.endswith('.csv'):
+        from user_agents import parse
+
+        # Generate CSV file from database records
+        query = Record.query.order_by(Record.timestamp.desc()).all()
+        data = []
+        for record in query:
+            agent= parse(record.user_agent)
+
+            data.append({
+                'id': record.id,
+                'timestamp': record.timestamp,
+                'ip_address': record.ip_address,
+                'user_agent': record.user_agent,
+                'os': agent.os.family,
+                'os_version': agent.os.version_string,
+                'device': agent.device.family,
+                'device_brand': agent.device.brand,
+                'device_model': agent.device.model,
+                'browser': agent.browser.family,
+                'browser_version': agent.browser.version_string,
+            })
+
+        from io import StringIO, BytesIO
+        import csv
+        output_text  = StringIO()
+        writer = csv.DictWriter(output_text, fieldnames=data[0].keys(), delimiter=';')
+        writer.writeheader()
+        writer.writerows(data)
+        output_text .seek(0)
+        output = BytesIO(output_text.read().encode('utf-8'))
+        output.seek(0)
+        return send_file(
+            output,
+            mimetype='text/csv',
+            as_attachment=True,
+            download_name=filename
+        )
+
+    return send_from_directory(directory=app.config['DOWNLOAD_FOLDER'], path=filename, as_attachment=True)
+
+
+@app.route('/reset_database', methods=['POST'])
+def reset_database():
+    pin = request.form.get('pin')
+    hardcoded_pin = "1234"  # PIN hardcoded para confirmação
+
+    if pin == hardcoded_pin:
+        try:
+            db.drop_all()
+            db.create_all()
+            #return "Database reset successfully!", 200 
+        except Exception as e:
+            return f"Error resetting database: {str(e)}", 500
+    else:
+        return "Invalid PIN. Database reset aborted.", 403
+
+    return redirect(url_for('view_records'))
+
+
+@app.route('/view/<int:page>')
+@app.route('/view/')
+@no_cache
+def view_records(page=1):
+    PAGE_SIZE = 10
+    slice_i = (page - 1) * PAGE_SIZE
+    slice_f = page * PAGE_SIZE
+
+    total_records = Record.query.count()
+    query = Record.query.order_by(Record.timestamp.desc()).slice(slice_i, slice_f).all()
+
+    pagination = {
+        'next': page + 1 if total_records > page * PAGE_SIZE else None,
+        'prev': page - 1 if page > 1 else None ,
+        'curr': page,
+        'pages': (total_records // PAGE_SIZE) + 1,
+        'records': total_records
+    }
+    return render_template('view.html', pagination=pagination, records=query)
+
+if __name__ == '__main__':
+    with app.app_context():
+        #db.drop_all()
+        print('Creating database tables...')
+        db.create_all()
+    
+    print('DEBUG?', app.config.get('DEBUG'))
+    app.run(host='0.0.0.0', port=5000, debug=app.config.get('DEBUG', False))
---
+++ flaskapp/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Runs the aplication
+if [ $? -eq 0 ]
+then
+  # Executa o programa
+  #echo Upgrade 
+  #pip install -r requirements.txt --no-cache-dir --upgrade
+  
+  echo Running App: $1
+  #gunicorn -w 10 -b :5000 run:app
+  python $1
+
+fi
